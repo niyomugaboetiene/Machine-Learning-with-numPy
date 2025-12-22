ecee27a4226a3b529901de5176fb4df982f41a6a
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-12-22 08:01:15 +0200

    learning gradient descent

--- gradient_descent/gradient_descent.py
+++ gradient_descent/gradient_descent.py
@@ -4,19 +4,19 @@ def gradient_descent(x, y):
     m_curr = b_curr = 0
     iteration = 1000
     n = len(x)
-    learning_rate = 0.001
+    learning_rate = 0.08
   
     for i in range(iteration):
         y_predicted = m_curr * x + b_curr
         md = -(2/n) * sum(x * (y - y_predicted))
-        bd = -(2/n) * sum(x*(y - y_predicted))
+        bd = -(2/n) * sum(y - y_predicted)
         # const = 1/n(sum(y - y_predicted)**2)
         cost = (1/n) * sum([val ** 2 for val in (y - y_predicted)])
         # m = m - learning_rate * m derivative
         m_curr = m_curr - learning_rate * md
         # m = b - learning_rate * b derivative
         b_curr = b_curr - learning_rate * bd
-        print("m {} b {}, iteration {}".format(m_curr, b_curr, i))
+        print("m {} b {}, cost {}, iteration {}".format(m_curr, b_curr, cost , i))
 
   
 x = np.array([1, 2, 3, 4, 5])
